d36baf0afaeea03b90b160c9c3ccfc9bc1bed6cf
An Phan <aanltll2992@gmail.com>
2019-01-03 16:54:33 +0700

    zzzzzzzzzzzzzzzz

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 const _GO = () => {
   chrome.tabs.executeScript(null, { file: "getPagesSource.js" }, () => {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    // var message = document.querySelector('#message');
+    var message = document.querySelector('#message');
     if (chrome.runtime.lastError)
       message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
   });
